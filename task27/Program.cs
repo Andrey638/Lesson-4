d43c3e9ff28499c03f9015cf3175acd1b301d3f5
Andrey3544 <box4435@mail.ru>
2022-12-12 22:06:24 +0300

    task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите число");
 int number=Convert.ToInt32(Console.ReadLine());
 int sum=0;
-for (int i=0;number>0;i++)
+for (int i=0;i<number;i++)
 {
     sum=sum+number%10;
     number/=10;
